92286a665d08af2d8c978cac1e75b56914f2e7a3
Ewing <stewing@amazon.com>
2018-11-15 12:55:10 -0500

    Misc updates.

--- keyboard/MD_1.1_Standard/log/buildvars.h
+++ keyboard/MD_1.1_Standard/log/buildvars.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013-2016 by Jacob Alexander
+/* Copyright (C) 2013-2017 by Jacob Alexander
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,25 +27,38 @@
 
 // ----- Defines -----
 
+// Helpers
+#define _STR_HELPER(x)
+#define _STR(x) _STR_HELPER(x)
+
 // You can change these to give your code its own name.
 #define STR_MANUFACTURER        L"Input Club"
-#define STR_PRODUCT             L"Keyboard - MD1.1 PartialMap pjrcUSB full"
-#define STR_SERIAL              L"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - mk20dx128vlf5"
+#define STR_PRODUCT             L"Keyboard - Infinity_60_LED:alphabet PixelMap USB"
+#define STR_SERIAL              L"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - mk20dx128"
+#define STR_CONFIG_NAME         L"xXXx"
 
 
 // Strings used in the CLI module
-#define CLI_Revision            "220632184329f06a4f9f8e6a5317d23e7305bd7e"
+#define CLI_Revision            "3d12be5727b2d68b66faa5c81c9ddbaf339dfda4"
+#define CLI_RevisionNumber      809
+#define CLI_RevisionNumberStr   "809"
+#define CLI_Version             "untagged-dd071079752b87a7a820"
+#define CLI_VersionRevision     "929a788d122e8ab984edecae9c22b882b95550a0"
+#define CLI_VersionRevNumber    706
+#define CLI_VersionRevNumberStr "706"
 #define CLI_Branch              "master"
 #define CLI_ModifiedStatus      "Clean"
 #define CLI_ModifiedFiles       "\r\n\t"
 #define CLI_RepoOrigin          "https://github.com/kiibohd/controller.git"
-#define CLI_CommitDate          "2017-02-20 16:38:15 -0800"
+#define CLI_CommitDate          "2018-02-20 00:38:06 -0800"
 #define CLI_CommitAuthor        "Jacob Alexander <haata@kiibohd.com>"
-#define CLI_Modules             "Scan(MD1.1) Macro(PartialMap) Output(pjrcUSB) Debug(full)"
-#define CLI_BuildDate           "2017-03-26 14:04:47 +0000"
-#define CLI_BuildOS             "Linux-4.1.5-x86_64-linode61"
+#define CLI_Modules             "Scan(Infinity_60_LED:alphabet) Macro(PixelMap) Output(USB) Debug(full)"
+#define CLI_BuildDate           "2018-02-24 02:55:30 +0000"
+#define CLI_BuildOS             "Ubuntu 14.04.5 LTS trusty"
+#define CLI_BuildCompiler       "/usr/bin/arm-none-eabi-gcc "
 #define CLI_Arch                "arm"
 #define CLI_Chip                "mk20dx128vlf5"
+#define CLI_ChipShort           "mk20dx128"
 #define CLI_CPU                 "cortex-m4"
 #define CLI_Device              "Keyboard"
 
@@ -56,5 +69,5 @@
 // match the INF file.
 #define VENDOR_ID               0x1C11
 #define PRODUCT_ID              0xB04D
-#define BCD_VERSION             509
+#define BCD_VERSION             809
 
