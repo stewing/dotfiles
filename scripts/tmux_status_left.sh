ad7c949dc6ef24f9e4056def3a96e24d404a780d
🚀 Steven Ewing 🌌 <steve@polysign.io>
2019-08-14 14:06:33 -0700

    Adding in script for left status.

---
+++ scripts/tmux_status_left.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+width=100
+
+branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+repo=$(basename $(git rev-parse --show-toplevel 2>/dev/null) 2>/dev/null)
+
+status=""
+
+if [ -n "$branch" ] ; then
+    status=$(printf " %s  %s" "$repo" "$branch")
+else
+    status=$(printf " %s  %s" "-" "-")
+fi
+
+status="$status   $(pwd)"
+
+printf "%*s" $width "$status"
