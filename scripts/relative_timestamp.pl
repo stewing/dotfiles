40c5bf7c6cc122918a364edcab79d5654cef9061
🚀 Steven Ewing 🌌 <steven.ewing@coinbase.com>
2020-07-06 16:54:30 -0700

    Add in some coloration for relative timestamps.

--- scripts/relative_timestamp.pl
+++ scripts/relative_timestamp.pl
@@ -1,18 +1,41 @@
 #!/usr/bin/perl
 
-use Time::HiRes qw(tv_interval gettimeofday);
+use Time::HiRes qw/tv_interval gettimeofday/;
+use Term::ANSIColor qw(:constants);
+use Statistics::Basic qw/stddev average/;
 
 my $td = 0;
 
 BEGIN {
     @lastcmdtime = gettimeofday();
     @cmdtime = gettimeofday();
+    @deltas = [1];
 }
 
 @cmdtime = gettimeofday();
 
 $td = tv_interval(\@lastcmdtime);
 
-printf "%0.6f: ", $td;
+$avg = average(@deltas);
+$stddev = stddev(@deltas);
+push(@deltas, $td);
 
-@lastcmdtime = @cmdtime
+
+$tddev = abs($avg - $td);
+
+if ($tddev < $avg) {
+    print BRIGHT_GREEN
+} elsif ($tddev <= $stddev) {
+    print GREEN
+} elsif ($tddev <= $stddev * 2) {
+    print YELLOW
+} elsif ($tddev <= $stddev * 4) {
+    print RED
+} elsif ($tddev > $stddev * 4) {
+    print BRIGHT_RED
+}
+
+printf "%04.6f: ", $td;
+print RESET;
+
+@lastcmdtime = @cmdtime;
