88ecde6d8cdc563fc4dbdb4cce5ea2f99f1846b2
🚀 Steven Ewing 🌌 <steven.ewing@coinbase.com>
2020-10-26 15:06:01 -0700

    Fix up nvim/coc config.

--- link.sh
+++ link.sh
@@ -6,11 +6,13 @@ ln -f bash.functions $HOME/.bash.functions
 
 ln -f zshrc $HOME/.zshrc
 
+# vim/nvim
 ln -f vimrc  $HOME/.vimrc
 mkdir -p $HOME/.config/nvim
 ln -f init.vim $HOME/.config/nvim/init.vim
+ln -f coc-settings.json  $HOME/.config/nvim/
+ln -f coc.vim  $HOME/.config/nvim/
 
-ln -f vimrc.coc  $HOME/.vimrc.coc
 ln -f tmux.conf $HOME/.tmux.conf
 ln -f alacritty.yml $HOME/.alacritty.yml
 ln -f chunkwmrc $HOME/.chunkwmrc
